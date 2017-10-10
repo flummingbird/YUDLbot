d91ba402632f2ab7ba2f5c46c87ad3fb5d6f9af5
flummingbird <wwconlin@gmail.com>
2017-10-10 12:59:57 -0500

    getting urllib.request working, no need to check descript with twitter metatags

--- YUDLbot.py
+++ YUDLbot.py
@@ -7,32 +7,22 @@ auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_
 auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
-response = urllib.urlopen(config.SOLR_DATA);
-data = json.loads(response.read())
-docs = data["response"]["docs"]
-
+response = urllib.request.urlopen(config.SOLR_DATA);
+data = response.read()
+decoded = data.decode('utf-8')
+d_json = json.loads(decoded)
+docs = d_json["response"]["docs"]
 items = random.sample(docs,1)
 
-response = urllib.urlopen(solr_data);
-data = json.loads(response.read())
-docs = data["response"]["docs"]
 
-items = random.sample(docs,1)
 
 for item in items:
   pid = item["PID"]
-  description = item["mods_abstract_s"][0]
-  if len(description) > 117:
-    while len(description) + 3 > 116:
-        description = description[:len(description) - 1]
-    description = description + '...'
-  title = item["mods_titleInfo_title_s"][0]
-  if len(title) > 117:
-    while len(title) + 3 > 116:
-        title = title[:len(title) - 1]
-    title = title + '...'
-  url = "http://digital.library.yorku.ca/islandora/object/"
+  print(pid + '\n')
+  url = "https://louisianadigitallibrary.org/islandora/object/"
   url += pid
-  tweet_text = "%s %s" % (description,url)
-  api.update_status(tweet_text)
-
+  if(urllib.request.urlopen(url).getcode() == 200):
+    print('tweeted')
+    api.update_status(status=url)
+  else:
+    print('failed')
