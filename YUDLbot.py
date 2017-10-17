7fd577078f0a8d4c491cf1e44361569c3b625066
flummingbird <wwconlin@gmail.com>
2017-10-17 16:03:37 -0500

    improvement.

--- YUDLbot.py
+++ YUDLbot.py
@@ -7,6 +7,10 @@ auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_
 auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
 api = tweepy.API(auth)
 
+print(config.SOLR_DATA)
+
+
+
 response = urllib.request.urlopen(config.SOLR_DATA);
 data = response.read()
 decoded = data.decode('utf-8')
@@ -15,11 +19,10 @@ docs = d_json["response"]["docs"]
 items = random.sample(docs,1)
 
 
-
 for item in items:
   pid = item["PID"]
   print(pid + '\n')
-  url = "https://louisianadigitallibrary.org/islandora/object/"
+  url = "http://louisianadigitallibrary.org/islandora/object/"
   url += pid
   if(urllib.request.urlopen(url).getcode() == 200):
     print('tweeted')
