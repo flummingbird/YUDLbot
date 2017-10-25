bb05f9a77d326ac4a793c454cb3c9148c97446ff
flummingbird <wwconlin@gmail.com>
2017-10-25 13:58:32 -0500

    solr subject search, and print list of twitter trends

---
+++ trend-YUDLbot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys, json, urllib, random, config, solr_config
+
+auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+trends =  api.trends_available()
+
+
+#t_data = trends.read()
+#t_decode = t_data.decode('utf-8')
+#t_json = json.loads(t_decode)
+
+
+
+print(trends)
+
+#response = urllib.request.urlopen(solr_config.SOLR_DATA);
+#data = response.read()
+#decoded = data.decode('utf-8')
+#d_json = json.loads(decoded)
+#docs = d_json["response"]["docs"]
+#items = random.sample(docs,1)
+
+
+#for item in items:
+#  pid = item["PID"]
+#  print(pid + '\n')
+#  url = "http://louisianadigitallibrary.org/islandora/object/"
+#  url += pid
+#  if(urllib.request.urlopen(url).getcode() == 200):
+#    print('tweeted')
+#    api.update_status(status=url)
+#  else:
+#    print('failed')
