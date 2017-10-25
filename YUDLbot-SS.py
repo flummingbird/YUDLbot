474543f0074e32eb6deebcc779f5a1e959ee0a2c
flummingbird <wwconlin@gmail.com>
2017-10-25 11:28:21 -0500

    subject search, new config options

---
+++ YUDLbot-SS.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys, json, urllib, random, config
+
+auth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET)
+auth.set_access_token(config.TWITTER_ACCESS_KEY, config.TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+
+
+response = urllib.request.urlopen(config.SUBJECT_SOLR_DATA);
+print(response)
+data = response.read()
+decoded = data.decode('utf-8')
+d_json = json.loads(decoded)
+docs = d_json["response"]["docs"]
+items = random.sample(docs,1)
+
+
+for item in items:
+  pid = item["PID"]
+  print(pid + '\n')
+  url = "http://louisianadigitallibrary.org/islandora/object/"
+  url += pid
+  if(urllib.request.urlopen(url).getcode() == 200):
+    print('tweeted')
+    api.update_status(status=url)
+  else:
+    print('failed')
