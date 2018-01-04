a44e572f9926688013701d58627628393413c238
flummingbird <wwconlin@gmail.com>
2018-01-04 09:18:32 -0600

    solr row should be rows

--- solr_config.py
+++ solr_config.py
@@ -1,21 +1,24 @@
+#simple config file that builds up the solr query. need to make this a bit more changeable
+
 import secret_var, random
 URL = secret_var.URL
-#QUERY = 'q=mods_abstract_mt%3A*'
+
 QUERY = 'q=dc.title%3A*'
 SUB_QUERY = 'q=dc.subject%3A'
 SUBJECT = 'louisiana'
+
+#QUERY = 'q=mods_abstract_mt%3A*'
 #QUERY = 'q=dc.title%3Atugboat'
 #QUERY = 'q=dc.title%3Aschooner'
 #QUERY = 'q=mods_subject_topic_mt%3Agoats'
 #QUERY = 'q=mods_subject_topic_ms%3A%22Louisiana%22'
-#&fl=PID%2C+mods_titleInfo_title_ms&wt=json&indent=true&row=1
+#&fl=PID%2C+mods_titleInfo_title_ms&wt=json&indent=true&rows=1
 #QUERY = 'q=goat'
-
 # NOT = '+AND+NOT+'
 #QUERY =QUERY + NOT
 
 REQ = '+AND+RELS_EXT_hasModel_uri_ms%3A%22info%3Afedora%2Fislandora%3Asp_large_image_cmodel%22&fl=PID%2C+dc.description+mods_titleInfo_title_ms+mods_abstract_mt&wt=json&indent=true'
-ROW = '&row=100'
+ROW = '&rows=100'
 START = '&start='
 RAND = str(random.randint(0, 239379))
 
@@ -24,5 +27,9 @@ S_RAND = str(random.randint(0, 1000))
 SOLR_DATA  = URL + QUERY + REQ + ROW + START + RAND
 
 SUBJECT_SOLR_DATA = URL + SUB_QUERY + SUBJECT + REQ + START + S_RAND
+
+HEAD = URL + 'q='
+
+TAIL = REQ + ROW + START + RAND
 
 print(SOLR_DATA)
